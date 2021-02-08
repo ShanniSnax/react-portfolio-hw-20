01ef01313d2e03323efb29ff6cfbc37da269ca73
Snax <shanni.c.suissa@gmail.com>
2021-02-07 23:24:17 -0800

    added router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/HOME";
+import Portfolio from "./pages/Portfolio";
 
 
 function App() {
   return (
     <Router>
-      <div>
-       
-      </div>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/portfolio' component={Portfolio} />
+        <Route path='/contact' component={Contact} />
+      </Switch>
     </Router>
   );
 }
